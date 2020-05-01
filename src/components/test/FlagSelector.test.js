18ea23e4daa078f5b959a211325b55e75caca484
FairSuspect <asasntv@gmail.com>
2020-05-01 12:57:36 +0300

    faulty test for checking actions

--- src/components/test/FlagSelector.test.js
+++ src/components/test/FlagSelector.test.js
@@ -48,7 +48,7 @@ it('getImageURL works correctly',() =>{
     const data = Data;
     const component = renderer.create(<FlagSelector />);
     renderer.act(() => {
-      expect(data).toHaveLength(67);
+      expect(data).toHaveLength(-150);
     });
   
     
